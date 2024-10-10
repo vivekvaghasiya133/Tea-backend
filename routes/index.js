4e2182fcdb2fc8585900dd22e39c8a51676a899a
mitali mori <mitalimori33@gmail.com>
2024-10-10 15:01:36 +0530

    Order

--- routes/index.js
+++ routes/index.js
@@ -53,7 +53,7 @@ router.put('/tea-orders/:id', teaOrderController.updateTeaOrder);
 // Route to delete a tea order by ID
 router.delete('/tea-orders/:id', teaOrderController.deleteTeaOrder);
 
-router.delete("/tea-status/:id", teaOrderController.updateTeaOrderStatus);
+router.patch("/tea-status/:id", teaOrderController.updateTeaOrderStatus);
 
 
 
